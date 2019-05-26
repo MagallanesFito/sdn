7832474d38282f3a220f8b7797df14df2130ecc3
MagallanesFito <magallanesfito@gmail.com>
2019-05-26 11:04:25 -0700

    Agregado floyd warshall

--- Algorithm.py
+++ Algorithm.py
@@ -2,39 +2,49 @@
 #Grafo de prueba
 G = [(1,2),(2,1),(1,3),(3,1),(3,4),(4,3),(2,4),(4,2),(1,4),(4,1)]
 #G  = [(1, 2), (2, 1), (3, 2), (2, 3)]
-
-class GraphAlgoritm:
+INF = (2<<32)
+class GraphAlgorithm:
         def __init__(self,g):
             #Representacion en lista de adyacencia
-            self.adjency_list = self.buildAdjencyList(g)
+            self.adjency_list = self.build_adjency_list(g)
             self.all_paths_list = [] 
             #numero de nodos del grafo
-            self.V = self.getNumNodes(g)
+            self.V = self.get_num_nodes(g)
             #Representacion en matriz de adyacencia
             self.adjency_matrix = self.adjency_list_to_matrix()
-            self.INF = 1000000
+            #self.INF = 1000000
         ''' getter de array de todas las rutas'''
         def get_all_paths(self):
             return self.all_paths_list
+        ''' Funcion auxiliar para convertir una lista de  
+        adyacencia en una matriz de adyacencia'''
         def adjency_list_to_matrix(self):
-            matrix = [[self.INF for i in range(self.V)] for j in range(self.V)]
+            matrix = [[INF for i in range(self.V)] for j in range(self.V)]
             for key,value in self.adjency_list.items():
                 for node in value:
                     matrix[key-1][node-1] = 1
+            for i in range(self.V):
+                matrix[i][i] = 0
             return matrix
+        '''Funcion auxiliar para imprimir una matriz de adyacencia'''
         def print_adjcency_matrix(self):
             for i in range(self.V):
                 for j in range(self.V):
                     print(str(self.adjency_matrix[i][j])+" ",end='')
                 print("\n")
-        def getNumNodes(self,g):
+        '''Dado un grafo representado como una lista de vertices
+        obtener el numero de nodos en el grafo.
+        '''
+        def get_num_nodes(self,g):
                 max_ = 0
                 for e in g:
                         maxLocal = max(e)
                         if maxLocal > max_:
                                 max_ = maxLocal
                 return max_
-        def buildAdjencyList(self,g):
+        ''' Dado un grafo representado como un conjunto de 
+        aristas, convertirlo en una lista de adyacencia'''
+        def build_adjency_list(self,g):
                 adjList = {}
                 for edge in g:
                         a = edge[0]
@@ -61,8 +71,7 @@ class GraphAlgoritm:
                         new_path.append(adjacent)
                         queue.append(new_path)
                     visited.add(node)
-        ''' Realiza una dfs desde el origen src
-        hasta destino dest'''
+        ''' Obtener todas las rutas de un vertice a otro'''
         def all_paths_util(self,src,dest,visited,path):
             visited.add(src)
             path.append(src)
@@ -82,25 +91,38 @@ class GraphAlgoritm:
             #adjency_list = self.buildAdjencyList(g)
             self.all_paths_util(src,dest,visited,path)
             return self.all_paths_list
-        ''' Realiza el algoritmo de floyd warshall para 
-        obtener una matriz de rutas mas cortas en O(V^3)'''
-        def all_pair_shortest_path(self):
-            self.distances = map(lambda i:map(lambda j:j,i),self.adjency_matrix)
-            
-            for k in range(self.V):
-                for i in range(self.V):
-                    for j in range(self.V):
-                        self.distances[i][j] = min(self.distances[i][j],self.distances[i][k]+self.distances[k][j])
-            #Imprime solucion
+        ''' Funcion auxiliar para imprimir una matriz de distancias
+        mas cortas.'''
+        def print_solution(self,matrix):
             for i in range(self.V):
                 for j in range(self.V):
-                    if self.distances[i][j] == self.INF:
+                    if self.distances[i][j] == INF:
                         print("INF ",end='')
                     else: 
                         print(str(self.distances[i][j])+" ",end='')
                 print("\n")
-algo  = GraphAlgoritm(G)
-algo.all_pair_shortest_path()
+        ''' Realiza el algoritmo de floyd warshall para 
+        obtener una matriz de rutas mas cortas entre todos
+        los pares de nodos en O(V^3)'''
+        def all_pairs_shortest_path(self):
+            distances = []
+            #Inicializa la matriz de distances
+            for i in range(self.V):
+                distances.append([])
+                for j in range(self.V):
+                    distances[i].append(self.adjency_matrix[i][j])
+            #Realiza la programacion dinamica
+            for k in range(self.V):
+                for i in range(self.V):
+                    for j in range(self.V):
+                        distances[i][j] = min(distances[i][j],distances[i][k]+distances[k][j])
+            #Imprime solucion
+            #self.print_solution(self.distances)
+            return distances
+            
+#algo  = GraphAlgorithm(G)
+#distances = algo.all_pairs_shortest_path()
+#print(distances)
 #src = 1
 #dest = 4
 
